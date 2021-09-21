89bce67f7f21ab1c640b82b37b0104d6e70f5de2
mkivi2 <mkivi@nd.edu>
2021-09-21 13:54:20 -0500

    updating the random things I have done

---
+++ R/clean_spline.R
@@ -0,0 +1,93 @@
+#' Filters data using a weighted least-squares linear regression model and a 
+#' moving window (aims to minimize squared error).
+#'
+#' @param orig_df A data frame created from the Beck's dataset.
+#' @param ID_list A list of all unique IDs within the Beck's dataset.
+#' @return A data frame of Beck's data w/o divergent data points.
+#' @export
+clean_WLS <- function(orig_df) {
+  
+  require(splines)
+  
+  # Add original row numbers as a column
+  orig_df = orig_df %>% 
+    rownames_to_column('orig_row') %>%
+    mutate(orig_row = as.numeric(orig_row))
+  
+  new_df = orig_df[1,]
+  
+  ID_list = unique(orig_df$ID)
+  pb = txtProgressBar(0, 1, style=3)
+  
+  # Iterate through all sites for which data was collected (data with same ID).
+  for (i in 1:length(ID_list)) {
+    setTxtProgressBar(pb, i/length(ID_list))
+    
+    # Data frame with data for ID `ID_list[i]`.
+    this = orig_df %>% dplyr::filter(ID == ID_list[i]) 
+    
+    # Remove any values before April with NDVI values greater than 0.3.
+    # Here I'm using the range given for bare soil in Mzid et al. (2021)
+    bad <- this %>% dplyr::filter(Date < as.Date('2017-06-01') & NDVI > 0.3)
+    if (nrow(bad) > 0) this <- this %>% dplyr::filter(!(orig_row %in% bad$orig_row))
+    
+    if (nrow(this) < 4) next
+    
+    # Convert from formatted date to Julian days. 
+    this$DOY <- as.numeric(difftime(this$Date, as.Date("2016-12-31","%Y-%m-%d"), units="days"))
+    
+    # Perform cubic spline interpolation
+    fit1<-smooth.spline(this$DOY,this$NDVI)
+    
+    #Plotting smoothing splines 
+    #plot(this$DOY,this$NDVI,col="grey",xlab="DOY",ylab="NDVI")
+    #lines(fit1, col = 'black')
+    #Sys.sleep(3)
+    
+    # Adjust for peak NDVI value 
+    fit1$y[which.max(this$NDVI)] = this$NDVI[which.max(this$NDVI)]
+    this$NDVI = fit1$y
+    
+    new_df = rbind(new_df, 
+                   this %>% dplyr::select(-DOY))
+
+    
+    # # predictor variable: date
+    # # explanatory variable: NDVI
+    # model <- lm(NDVI ~ Date, data = this)
+    # summary(model)
+    # 
+    # # Plot:
+    # plot(fitted(model), resid(model), xlab='Fitted Values', ylab='Residuals')
+    # abline(0,0)
+    # 
+    # # Calculate weight
+    # wt <- 1 / lm(abs(model$residuals) ~ model$fitted.values)$fitted.values^2
+    # wls_model <- lm(NDVI ~ Date, data = this, weights=wt)
+    # 
+    # summary(wls_model)
+    # plot(fitted(wls_model), resid(wls_model), xlab='Fitted Values', ylab='Residuals')
+    # abline(0,0)
+    # 
+    # residuals <- residuals(wls_model)
+    # 
+    # # What I don't understand here is how the "moving window" idea works ^^
+    # # I.e., is the WLS Model recalculated at every data point?
+    # 
+    # # Iterate through all data points.
+    # for (j in first:last) { 
+    #   # Remove points with residuals greater than some value (0.3? 0.4?)
+    #   resid <- residuals[[j]]
+    #   
+    # }
+    # 
+    # p = ggplot() + 
+    #   geom_point(data = this, aes(x = Date, y = NDVI), col = 'black') +
+    #   geom_line(data = this, aes(x = Date, y = NDVI), col = 'black')
+    
+    # orig_df = orig_df %>% filter(!orig_row %in% rem_rows)
+  }
+  
+  new_df = new_df[-1,]
+  return(new_df)
+}
--- inst/cleaning_workflow.R
+++ inst/cleaning_workflow.R
@@ -17,48 +17,49 @@ temp <- read.csv('inst/data/cleanedData.csv') %>%
 
 # Remove any sites with late DOS values or with DOS values after harvest (dataset errors).
 temp <- temp %>% filter(dos < 182, dos < doh)
+# Remove any non-corn sites (corn is 'Family 1').
+temp = temp %>% filter(Family == 1)
 
 # Let's check to see how the cleaning function works
-#ids = sample(unique(temp$ID), 12, replace=F)
-#before = temp %>% filter(ID %in% ids)
-#before$check = 'before'
+set.seed(102396)
+ids = sample(unique(temp$ID), 12, replace=F)
+before = temp %>% filter(ID %in% ids)
+before$check = 'before'
 
 # Remove any data points that are not monotonically increasing/decreasing 
 IDs = unique(temp$ID)
 
 if (!file.exists('inst/data/monotonic_corn_data.Rdata')){
-  temp <- clean_monotonic(orig_df = temp)
+  temp <- clean_WLS(orig_df = temp)
   
   # Remove any sites with fewer than 5 data points.
   finalIDs = c()
   for (i in 1:length(IDs)) {
-    this <- temp %>% filter(ID == IDs[i])
+    this <- temp %>% dplyr::filter(ID == IDs[i])
     if (nrow(this) >= 5)
       finalIDs = c(finalIDs, IDs[i])
   }
-  temp <- temp %>% filter(ID %in% finalIDs)
+  temp <- temp %>% dplyr::filter(ID %in% finalIDs)
   
-  # Remove any non-corn sites (corn is 'Family 1').
-  temp = temp %>% filter(Family == 1)
-  
-  save(temp, file = 'inst/data/monotonic_corn_data.Rdata')
+  save(temp, file = 'inst/data/spline_corn_data.Rdata')
 }else{
   load('inst/data/monotonic_corn_data.Rdata')
 }
 
 # Plot!
-#after = temp %>% filter(ID %in% ids)
-#after$check = 'after'
-#check = rbind(before,after) %>% mutate(Date = as.Date(Date))
-#ggplot(check)+
-#  geom_line(aes(x = Date, y = NDVI, color = check), alpha = 0.5) + 
-#  geom_point(aes(x = Date, y = NDVI, color = check)) + 
-#  facet_wrap(~ID, nrow = 4)
+after = temp %>% filter(ID %in% ids) %>% dplyr::select(-orig_row)
+after$check = 'after'
+check = rbind(before,after) %>% mutate(Date = as.Date(Date),
+                                       Sowing = as.Date(dos, origin = '2016-12-31'))
+ggplot(check)+
+  geom_line(aes(x = Date, y = NDVI, color = check), alpha = 0.5) + 
+  geom_point(aes(x = Date, y = NDVI, color = check)) +
+  geom_vline(aes(xintercept = Sowing))+ 
+  facet_wrap(~ID, nrow = 4)
 
 cornIDs = unique(temp$ID)
 
 # Randomly select training and test data.
-set.seed(102396)
 trainIDs = sample(cornIDs, length(cornIDs) / 2, replace = FALSE)
 train = temp %>% filter(ID %in% trainIDs)
 test = temp %>% filter(!(ID %in% trainIDs))
@@ -76,3 +77,4 @@ test = test %>% mutate(
 )
 
 save(test, train, file = 'inst/data/test_train_data.Rdata')
+
